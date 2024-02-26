a0e12c9544f1de0297a05191b00648fcbc364c5e
Usuario <Usuario@DESKTOP-LBJSDQI>
2024-02-26 01:14:26 -0600

    Agregar archivos de proyecto.

---
+++ webApiPractica/Controllers/equiposController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webApiPractica.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace webApiPractica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContext;
+
+        public equiposController(equiposContext equiposContext)
+        {
+            _equiposContext = equiposContext;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+
+        public IActionResult Get()
+        {
+            List<equipos> listadoEquipo = (from e in _equiposContext.equipos
+                                           select e).ToList();
+            if (listadoEquipo.Count() = 0)
+            {
+                return NotFound();
+
+            }
+            return OK (listadoEquipo);
+        }
+        
+    }
+}
---
+++ webApiPractica/Program.cs
@@ -0,0 +1,50 @@
+
+using Microsoft.EntityFrameworkCore;
+using webApiPractica.Models;
+
+namespace webApiPractica
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            //Inyección
+            builder.Services.AddDbContext<equiposContext>(options =>
+                options.UseSqlServer(
+                        builder.Configuration.GetConnectionString("equiposDbConnection")
+                    )
+            );
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+
+        }
+
+        
+    }
+}
